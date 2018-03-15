768c6a84636c77aa3aa9b813c1e10c5b2a9a2b4b
chenximin <chenximin@91tianmi.com>
2018-03-15 14:03:33 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
         <f7-view id="tab-1" main tab tab-active><news/></f7-view>
         <f7-view id="tab-2" tab><threads></threads></f7-view>
         <f7-view id="tab-3" tab><system></system></f7-view>
-        <f7-toolbar tabbar labels>
+        <f7-toolbar tabbar labels bottom-md>
           <f7-link icon-fa="fa fa-list" icon-size="25px" text="快讯" tab-link="#tab-1" tab-link-active></f7-link>
           <f7-link icon-fa="fa fa-comment-alt" icon-size="25px" text="币吧" tab-link="#tab-2"></f7-link>
           <f7-link icon-fa="fa fa-cog" icon-size="25px" text="设置" tab-link="#tab-3"></f7-link>
